7908e3e74ae7573a17e40efdb5cb502e30643404
Giuseppe Chaves Magnago <giuseppecm2014@gmail.com>
2024-09-11 17:24:50 -0300

    test

--- api/index.js
+++ api/index.js
@@ -45,21 +45,18 @@ export default async (req, res) => {
 
   if (blacklist.includes(username)) {
     return res.send(
-      `<html><body>${renderError(
-        "Something went wrong",
-        "This username is blacklisted", 
-        {
-          title_color,
-          text_color,
-          bg_color,
-          border_color,
-          theme,
-        })}</body></html>`
+      renderError("Something went wrong", "This username is blacklisted", {
+        title_color,
+        text_color,
+        bg_color,
+        border_color,
+        theme,
+      }),
     );
   }
 
   if (locale && !isLocaleAvailable(locale)) {
-    return res.send(`<html><body>${renderError("Something went wrong", "Language not found")}</body></html>`);
+    return res.send(renderError("Something went wrong", "Language not found"));
   }
 
   try {
@@ -91,77 +88,33 @@ export default async (req, res) => {
     );
 
     // Retorna o SVG dentro de uma estrutura HTML com um dropdown para selecionar idioma
-    return res.send(`
-      <html>
-        <body>
-          <div>
-            ${renderStatsCard(stats, {
-              hide: parseArray(hide),
-              show_icons: parseBoolean(show_icons),
-              hide_title: parseBoolean(hide_title),
-              hide_border: parseBoolean(hide_border),
-              card_width: parseInt(card_width, 10),
-              hide_rank: parseBoolean(hide_rank),
-              include_all_commits: parseBoolean(include_all_commits),
-              line_height,
-              title_color,
-              ring_color,
-              icon_color,
-              text_color,
-              text_bold: parseBoolean(text_bold),
-              bg_color,
-              theme,
-              custom_title,
-              border_radius,
-              border_color,
-              number_format,
-              locale: locale ? locale.toLowerCase() : null,
-              disable_animations: parseBoolean(disable_animations),
-              rank_icon,
-              show: showStats,
-            })}
-            
-
-            <!-- Adiciona o drop-down para selecionar o idioma -->
-            <label for="languageSelector" onload="checkLang()">Escolha o idioma:</label>
-            <select id="languageSelector">
-              <option value="" selected>Linguagem</option>
-              <option value="en">Inglês</option> <!-- Inglês -->
-              <option value="pt-br">Português</option> <!-- Português -->
-              <option value="fr">Francês</option> <!-- Francês -->
-              <option value="es">Espanhol</option> <!-- Espanhol -->
-              <option value="de">Alemão</option> <!-- Alemão -->
-              <option value="pl">Polonês</option> <!-- Polonês -->
-              <option value="ru">Russo</option> <!-- Russo -->
-              <option value="ar">Árabe</option> <!-- Árabe -->
-              <option value="ja">Japonês</option> <!-- Japonês -->
-              <option value="cn">Chinês</option> <!-- Chinês -->
-              <option value="np">Nepalês</option> <!-- Nepalês -->
-            </select>
-          </div>
-
-          <!-- Script para manipular o SVG -->
-          <script>            
-            // Função para atualizar o título do langcard com base na seleção de idioma
-            function atualizarIdioma() {
-              // Seleciona o SVG
-              const svg = document.querySelector('svg'); // Assume que o SVG é o único na página
-              const languageOption = document.getElementById('languageSelector').value;
-
-              if (languageOption !== "") {
-                // Recarrega a página com o novo idioma selecionado
-                const urlParams = new URLSearchParams(window.location.search);
-                urlParams.set('locale', languageOption);
-                window.location.search = urlParams.toString();
-              }
-            }
-
-            // Adiciona o evento de mudança ao drop-down
-            document.getElementById('languageSelector').addEventListener('change', atualizarIdioma);
-          </script>
-        </body>
-      </html>
-    `);
+    return res.send(
+      renderStatsCard(stats, {
+        hide: parseArray(hide),
+        show_icons: parseBoolean(show_icons),
+        hide_title: parseBoolean(hide_title),
+        hide_border: parseBoolean(hide_border),
+        card_width: parseInt(card_width, 10),
+        hide_rank: parseBoolean(hide_rank),
+        include_all_commits: parseBoolean(include_all_commits),
+        line_height,
+        title_color,
+        ring_color,
+        icon_color,
+        text_color,
+        text_bold: parseBoolean(text_bold),
+        bg_color,
+        theme,
+        custom_title,
+        border_radius,
+        border_color,
+        number_format,
+        locale: locale ? locale.toLowerCase() : null,
+        disable_animations: parseBoolean(disable_animations),
+        rank_icon,
+        show: showStats,
+      }),
+    );
   } catch (err) {
     res.setHeader(
       "Cache-Control",
@@ -171,18 +124,14 @@ export default async (req, res) => {
     ); // Use lower cache period for errors.
 
     // Retorna o erro também dentro de uma estrutura HTML
-    return res.send(`
-      <html>
-        <body>
-          ${renderError(err.message, err.secondaryMessage, {
-            title_color,
-            text_color,
-            bg_color,
-            border_color,
-            theme,
-          })}
-        </body>
-      </html>
-    `);
+    return res.send(
+      renderError(err.message, err.secondaryMessage, {
+        title_color,
+        text_color,
+        bg_color,
+        border_color,
+        theme,
+      }),
+    );
   }
 };
--- api/top-langs.js
+++ api/top-langs.js
@@ -40,21 +40,18 @@ export default async (req, res) => {
 
   if (blacklist.includes(username)) {
     return res.send(
-      `<html><body>${renderError(
-        "Something went wrong",
-        "This username is blacklisted", 
-        {
-          title_color,
-          text_color,
-          bg_color,
-          border_color,
-          theme,
-        })}</body></html>`
+      renderError("Something went wrong", "This username is blacklisted", {
+        title_color,
+        text_color,
+        bg_color,
+        border_color,
+        theme,
+      }),
     );
   }
 
   if (locale && !isLocaleAvailable(locale)) {
-    return res.send(`<html><body>${renderError("Something went wrong", "Locale not found")}</body></html>`);
+    return res.send(renderError("Something went wrong", "Locale not found"));
   }
 
   if (
@@ -62,7 +59,9 @@ export default async (req, res) => {
     (typeof layout !== "string" ||
       !["compact", "normal", "donut", "donut-vertical", "pie"].includes(layout))
   ) {
-    return res.send(`<html><body>${renderError("Something went wrong", "Incorrect layout input")}</body></html>`);
+    return res.send(
+      renderError("Something went wrong", "Incorrect layout input"),
+    );
   }
 
   try {
@@ -90,94 +89,26 @@ export default async (req, res) => {
     );
 
     // Retorna o SVG dentro de uma estrutura HTML
-    return res.send(`
-      <html>
-        <body>
-          <div>
-            ${renderTopLanguages(topLangs, {
-              custom_title,
-              hide_title: parseBoolean(hide_title),
-              hide_border: parseBoolean(hide_border),
-              card_width: parseInt(card_width, 10),
-              hide: parseArray(hide),
-              title_color,
-              text_color,
-              bg_color,
-              theme,
-              layout,
-              langs_count,
-              border_radius,
-              border_color,
-              locale: locale ? locale.toLowerCase() : null,
-              disable_animations: parseBoolean(disable_animations),
-              hide_progress: parseBoolean(hide_progress),
-            })}
-
-            <!-- Adiciona o drop-down para selecionar o idioma -->
-            <label for="languageSelector">Escolha o idioma:</label>
-            <select id="languageSelector">
-              <option value="en">Inglês</option> <!-- Inglês -->
-              <option value="pt">Português</option> <!-- Português -->
-              <option value="fr">Francês</option> <!-- Francês -->
-              <option value="es">Espanhol</option> <!-- Espanhol -->
-              <option value="de">Alemão</option> <!-- Alemão -->
-              <option value="pl">Polonês</option> <!-- Polonês -->
-              <option value="ru">Russo</option> <!-- Russo -->
-              <option value="ar">Árabe</option> <!-- Árabe -->
-              <option value="ja">Japonês</option> <!-- Japonês -->
-              <option value="cn">Chinês</option> <!-- Chinês -->
-              <option value="np">Nepalês</option> <!-- Nepalês -->
-            </select>
-          </div>
-
-          <!-- Script para manipular o SVG -->
-          <script>
-            // Função para atualizar o título do langcard com base na seleção de idioma
-            function atualizarTituloIdioma() {
-              // Seleciona o SVG
-              const svg = document.querySelector('svg'); // Assume que o SVG é o único na página
-                
-              // Seleciona o elemento <text> que representa o título (assumindo que é o primeiro elemento <text>)
-              const titulo = svg.querySelector('text'); // Você pode ajustar o seletor se necessário
-                
-              // Verifica a seleção do drop-down (inglês ou português)
-              const languageOption = document.getElementById('languageSelector').value;
-
-              // Atualiza o título de acordo com a seleção de idioma
-              if (languageOption === 'en') {
-                titulo.textContent = "Most Used Languages";
-              } if (languageOption === 'pt') {
-                titulo.textContent = "Linguagens Mais Usadas";
-              } if (languageOption === 'fr') {
-                titulo.textContent = "Langages les plus utilisés";
-              } if (languageOption === 'es') {
-                titulo.textContent = "Lenguajes más usados";
-              } if (languageOption === 'de') {
-                titulo.textContent = "Meist verwendete Sprache";
-              } if (languageOption === 'pl') {
-                titulo.textContent = "Najczęściej używane języki";
-              } if (languageOption === 'ru') {
-                titulo.textContent = "Наиболее часто используемые языки";
-              } if (languageOption === 'ar') {
-                titulo.textContent = "أكثر اللغات إستخداماً";
-              } if (languageOption === 'ja') {
-                titulo.textContent = "最もよく使っている言語";
-              } if (languageOption === 'cn') {
-                titulo.textContent = "最常用的语言";
-              } if (languageOption === 'np') {
-                titulo.textContent = "अधिक प्रयोग गरिएको भाषाहरू";
-              }
-            }
-
-            // Adiciona o evento de mudança ao drop-down
-            document.getElementById('languageSelector').addEventListener('change', atualizarTituloIdioma);
-            
-            // Chama a função para atualizar o título ao carregar a página
-            atualizarTituloIdioma();
-          </script>
-        </body>
-      </html>
-    `);
+    return res.send(
+      renderTopLanguages(topLangs, {
+        custom_title,
+        hide_title: parseBoolean(hide_title),
+        hide_border: parseBoolean(hide_border),
+        card_width: parseInt(card_width, 10),
+        hide: parseArray(hide),
+        title_color,
+        text_color,
+        bg_color,
+        theme,
+        layout,
+        langs_count,
+        border_radius,
+        border_color,
+        locale: locale ? locale.toLowerCase() : null,
+        disable_animations: parseBoolean(disable_animations),
+        hide_progress: parseBoolean(hide_progress),
+      }),
+    );
   } catch (err) {
     res.setHeader(
       "Cache-Control",
@@ -187,18 +118,14 @@ export default async (req, res) => {
     );
 
     // Retorna o erro também dentro de uma estrutura HTML
-    return res.send(`
-      <html>
-        <body>
-          ${renderError(err.message, err.secondaryMessage, {
-            title_color,
-            text_color,
-            bg_color,
-            border_color,
-            theme,
-          })}
-        </body>
-      </html>
-    `);
+    return res.send(
+      renderError(err.message, err.secondaryMessage, {
+        title_color,
+        text_color,
+        bg_color,
+        border_color,
+        theme,
+      }),
+    );
   }
 };
